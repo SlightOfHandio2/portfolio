1456946376d4b4dc8ef2d5427bbeddd0e09fdac8
DellConagherEngineer <olliecockell@gmail.com>
2023-03-20 13:11:56 +0000

    text

--- src/subPages/About.tsx
+++ src/subPages/About.tsx
@@ -5,7 +5,17 @@ const About = () => {
   return (
     <div className='About'>
       <p className='Title'>About me:</p>
-
+      <p className='Text'>
+        Hello, I'm Oliver Cockell, currently a software engineer apprentice working at KPMG and studying for my degree at queen mary university.
+      </p>
+      <br/>
+      <p className='Text'>
+        With an avid interest in computer science and web devlopment I make a competent developer that would fit into any development team.
+      </p>
+      <br/>
+      <p className='Text'>
+        Feel free to reach out to me on linkedIn or through my email at olliecockell@gmail.com
+      </p>
     </div>
   );
 }
