af812cedb14c80aad817767e3f0e573277c2280d
DellConagherEngineer <olliecockell@gmail.com>
2024-07-16 20:26:34 +0100

    getting there

--- src/subPages/Skills.tsx
+++ src/subPages/Skills.tsx
@@ -5,20 +5,23 @@ import kpmgLogo from "../images/icons/kpmg.jpeg";
 const About = () => {
   return (
     <div className="About">
-      <p className="titleProject">Experience:</p>
+      <h2 className="titleProject noBottom">Experience:</h2>
       <hr className="Break" />
       <div className="employment">
-        
         <img className="logo" src={kpmgLogo} alt="KPMG" />
-        <div>
-        <p className="SubTitle">KPMG  September 2019 - Present</p>
-        <p className="Text">
-          Currently working at KPMG as a software engineer apprentice. I have
-          been working on a variety of projects including a react web app and
-          some automation scripts.
-        </p>
+        <div className="employmentDetails">
+          <h3 className="SubTitle">KPMG</h3>
+          <p className="Date">September 2019 - Present</p>
+          <p className="Text">
+            Currently working at KPMG as a software engineer apprentice. I have
+            been working on a variety of projects including a react web app and
+            some automation scripts.
+          </p>
         </div>
       </div>
+      <hr className="Break" />
+      <h2 className="titleProject noBottom">Qualifications:</h2>
+
     </div>
   );
 };
